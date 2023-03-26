989754f51c1dcaef7cdadefe5ce21fadf8dcfb84
Lou CHANCRIN <louchancrin@hotmail.com>
2023-03-26 16:36:06 +0200

    Implement carousel

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,101 +1,108 @@
 <script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const gameMods = [
+  {
+    name: "Flash Cards",
+    src: require('../assets/logo.jpg'),
+    description: "Flash cards description",
+    redirect: '/FlashCards'
+  },
+  {
+    name: "TBD mod",
+    src: require('../assets/logo.jpg'),
+    description: "Mod description",
+    redirect: '/ModRoute'
+  },
+  {
+    name: "TBD mod",
+    src: require('../assets/logo.jpg'),
+    description: "Mod description",
+    redirect: '/ModRoute'
+  },
+  {
+    name: "TBD mod",
+    src: require('../assets/logo.jpg'),
+    description: "Mod description",
+    redirect: '/ModRoute'
+  },
+  {
+    name: "TBD mod",
+    src: require('../assets/logo.jpg'),
+    description: "Mod description",
+    redirect: '/ModRoute'
+  },
+  {
+    name: "TBD mod",
+    src: require('../assets/logo.jpg'),
+    description: "Mod description",
+    redirect: '/ModRoute'
+  },
+  {
+    name: "TBD mod",
+    src: require('../assets/logo.jpg'),
+    description: "Mod description",
+    redirect: '/ModRoute'
+  },
+  {
+    name: "TBD mod",
+    src: require('../assets/logo.jpg'),
+    description: "Mod description",
+    redirect: '/ModRoute'
+  },
+  {
+    name: "TBD mod",
+    src: require('../assets/logo.jpg'),
+    description: "Mod description",
+    redirect: '/ModRoute'
+  }
+]
 </script>
 
 <template>
-  <div class="hello">
-    <h1>Home page</h1>
-      <v-card
-      class="mx-auto"
-      max-width="368"
-      variant="outlined"
+  <v-sheet
+    class="mx-auto"
+    elevation="8"
+  >
+    <v-slide-group
+      v-model="model"
+      class="pa-4"
+      selected-class="bg-success"
+      show-arrows
     >
-      <v-card-item title="Florida">
-        <template v-slot:subtitle>
-          <v-icon
-            icon="mdi-alert"
-            size="18"
-            color="error"
-            class="me-1 pb-1"
-          ></v-icon>
-
-          Extreme Weather Alert
-        </template>
-      </v-card-item>
-
-      <v-card-text class="py-0">
-        <v-row align="center" no-gutters>
-          <v-col
-            class="text-h2"
-            cols="6"
-          >
-            64&deg;F
-          </v-col>
-
-          <v-col cols="6" class="text-right">
-            <v-icon
-              color="error"
-              icon="mdi-weather-hurricane"
-              size="88"
-            ></v-icon>
-          </v-col>
-        </v-row>
-      </v-card-text>
-
-      <div class="d-flex py-3 justify-space-between">
-        <v-list-item
-          density="compact"
-          prepend-icon="mdi-weather-windy"
+      <v-slide-group-item
+        v-for="mod in gameMods"
+        :key="mod"
+      >
+        <v-card
+          background-color="#222222"
+          class="ma-4"
+          height="800"
+          width="320"
+          @click="router.push(mod.redirect)"
+          variant="outlined"
         >
-          <v-list-item-subtitle>123 km/h</v-list-item-subtitle>
-        </v-list-item>
+        
+          <v-img
+            cover
+            height="400"
+            :src="mod.src"
+          ></v-img>
 
-        <v-list-item
-          density="compact"
-          prepend-icon="mdi-weather-pouring"
-        >
-          <v-list-item-subtitle>48%</v-list-item-subtitle>
-        </v-list-item>
-      </div>
-
-      <v-expand-transition>
-        <div v-if="expand">
-          <div class="py-2">
-            <v-slider
-              v-model="time"
-              :max="6"
-              :step="1"
-              :ticks="labels"
-              class="mx-4"
-              color="primary"
-              density="compact"
-              hide-details
-              show-ticks="always"
-              thumb-size="10"
-            ></v-slider>
-          </div>
-
-          <v-list class="bg-transparent">
-            <v-list-item
-              v-for="item in forecast"
-              :key="item.day"
-              :title="item.day"
-              :append-icon="item.icon"
-              :subtitle="item.temp"
-            >
-            </v-list-item>
-          </v-list>
-        </div>
-      </v-expand-transition>
-
-      <v-divider></v-divider>
-
-      <v-card-actions>
-        <v-btn @click="expand = !expand">
-          {{ !expand ? 'Full Report' : 'Hide Report' }}
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </div>
+          <v-card-item>
+            <div>
+              <div class="text-h6 mb-1">
+                {{ mod.name }}
+              </div>
+              <div class="text-caption">{{ mod.description }}</div>
+            </div>
+          </v-card-item>
+        </v-card>
+      </v-slide-group-item>
+    </v-slide-group>
+  </v-sheet>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
